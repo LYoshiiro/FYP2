963c47b366dbd2c9897a5b4b76e49f0952c6d69c
アマチ ヨシイロ/Skariius Alstria <LYoshiiro@gmail.com>
2019-01-26 18:16:23 +0800

    Crafting Menu

--- FYP2/Assets/Scripts/Crafting.cs
+++ FYP2/Assets/Scripts/Crafting.cs
@@ -6,6 +6,15 @@ public class Crafting : MonoBehaviour {
 // Class Reference
 	[SerializeField] private Core rCore;
 	[SerializeField] private ItemManager rItemManager;
+	[SerializeField] private Transform rMenu;
+
+	private void LateUpdate() {
+	// Check if game is paused
+        if (rCore.bPause != true) {
+			if (Input.GetKeyDown(KeyCode.C))
+				rMenu.gameObject.SetActive(!rMenu.gameObject.activeSelf);
+		}
+	}
 
 // Crafting Raft Function
 	public void CraftRaft() {
@@ -52,6 +61,56 @@ public class Crafting : MonoBehaviour {
 					rItemManager.GetItems().Find(i => i.sName == "Cotton").iCount -= 5;
 				// Add item
 					rItemManager.GetItems().Find(i => i.sName == "Cotton Pant").iCount += 1;
+				}
+			break;
+			
+		// Irregularity
+			default:
+				rCore.Pnt("No call value!");
+			break;
+		}
+	}
+
+// Clothing
+	public void CraftTools(int call) {
+	// Check which section is being called
+		switch (call) {
+		// Hoe
+			case 0:
+			// Check cost
+				if ((rItemManager.GetItems().Find(i => i.sName == "Wood").iCount >= 2) &&
+					(rItemManager.GetItems().Find(i => i.sName == "Stone").iCount >= 2)) {
+				// Reduce cost
+					rItemManager.GetItems().Find(i => i.sName == "Wood").iCount -= 2;
+					rItemManager.GetItems().Find(i => i.sName == "Stone").iCount -= 2;
+				// Add item
+					rItemManager.GetItems().Find(i => i.sName == "Hoe").iCount += 1;
+				}
+			break;
+		
+		// Axe
+			case 1:
+			// Check cost
+				if ((rItemManager.GetItems().Find(i => i.sName == "Wood").iCount >= 2) &&
+					(rItemManager.GetItems().Find(i => i.sName == "Stone").iCount >= 3)) {
+				// Reduce cost
+					rItemManager.GetItems().Find(i => i.sName == "Wood").iCount -= 2;
+					rItemManager.GetItems().Find(i => i.sName == "Stone").iCount -= 3;
+				// Add item
+					rItemManager.GetItems().Find(i => i.sName == "Axe").iCount += 1;
+				}
+			break;
+		
+		// Pickaxe
+			case 2:
+			// Check cost
+				if ((rItemManager.GetItems().Find(i => i.sName == "Wood").iCount >= 2) &&
+					(rItemManager.GetItems().Find(i => i.sName == "Stone").iCount >= 3)) {
+				// Reduce cost
+					rItemManager.GetItems().Find(i => i.sName == "Wood").iCount -= 2;
+					rItemManager.GetItems().Find(i => i.sName == "Stone").iCount -= 3;
+				// Add item
+					rItemManager.GetItems().Find(i => i.sName == "Pickaxe").iCount += 1;
 				}
 			break;
 			
