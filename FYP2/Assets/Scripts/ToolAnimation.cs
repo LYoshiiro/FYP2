7acd26bcdd0e5d89c4ccaf960bfbe014179b1e68
アマチ ヨシイロ/Skariius Alstria <LYoshiiro@gmail.com>
2019-01-27 22:59:08 +0800

    Tool Animated

--- FYP2/Assets/Scripts/ToolAnimation.cs
+++ FYP2/Assets/Scripts/ToolAnimation.cs
@@ -3,14 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ToolAnimation : MonoBehaviour {
+// Modifier
+	[SerializeField] private float fShift;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	private void Update() {
+	// Animate the tool to go up and down
+		fShift = Mathf.Sin(Time.time * 20.0f);
+		transform.Rotate(Vector3.forward, fShift);
 	}
 }
