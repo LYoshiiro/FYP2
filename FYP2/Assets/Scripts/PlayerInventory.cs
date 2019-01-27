322c334ffdf4b1e887907eab0e34ee92cdb28d8e
アマチ ヨシイロ/Skariius Alstria <LYoshiiro@gmail.com>
2019-01-27 18:50:22 +0800

    Inventory and some cleanup

---
+++ FYP2/Assets/Scripts/PlayerInventory.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerInventory : MonoBehaviour {
+// Class Reference
+	[SerializeField] private Core rCore;
+	[SerializeField] private ItemManager rItemManager;
+
+// Menu Variables
+	[SerializeField] private GameObject tItem;
+	[SerializeField] private Transform tContent;
+	[SerializeField] private Transform tMenu;
+	private Dictionary<int, Item> dItems;
+	private bool bMenuChange;
+
+	private void Start() {
+	// Set Initial Values
+		bMenuChange = true;
+		dItems = new Dictionary<int, Item>();
+	}
+
+	private void LateUpdate() {
+		foreach (Item itm in rItemManager.GetItems()) {
+		// Check if the Dictionary contains instance
+			if (!dItems.ContainsKey(itm.iID)) {
+				dItems.Add(itm.iID, itm);
+			}
+		// Check if the count matches
+			else if (dItems.ContainsKey(itm.iID)) {
+				if (dItems[itm.iID].iCount != itm.iCount) {
+					dItems[itm.iID] = itm;
+					bMenuChange = true;
+				}
+			}
+		}
+
+    // Check if game is paused
+        if (rCore.bPause != true) {
+		// Only Update if there is changes
+			if (bMenuChange) {
+			// Destroy all children first
+				foreach (Transform child in tContent)
+					GameObject.Destroy(child.gameObject);
+
+			// Create new Menu list
+				for (int i = 0; i < dItems.Count; i++) {
+					GameObject gItem = Instantiate(tItem, tContent) as GameObject;
+					gItem.name = dItems[i].sName;
+					gItem.transform.GetChild(0).GetComponent<Text>().text = "Name: " + dItems[i].sName;
+					gItem.transform.GetChild(1).GetComponent<Text>().text = "x" + dItems[i].iCount;
+					gItem.transform.GetChild(2).GetComponent<Text>().text = dItems[i].sNote;
+				}
+
+			// If key is called
+				if (Input.GetKeyDown(KeyCode.I))
+					tMenu.gameObject.SetActive(!tMenu.gameObject.activeSelf);
+			}
+		}
+	}
+}
--- FYP2/Assets/Scripts/UI.cs
+++ FYP2/Assets/Scripts/UI.cs
@@ -6,46 +6,17 @@ using UnityEngine.UI;
 public class UI : MonoBehaviour {
 // Class Reference
 	[SerializeField] private Core rCore;
-	[SerializeField] private ItemManager rItemManager;
 
 // List Reference
-	[SerializeField] private List<Text> lText;
-	[SerializeField] private List<Item> lItem;
+	[SerializeField] private Text tFPS;
 	[SerializeField] private List<Transform> lTransform;
 
 	private void FixedUpdate() {
     // Check if game is paused
         if (rCore.bPause != true) {
-		// Parse Data
-			lItem = rItemManager.GetItems();
-
 		// FPS
-			lText.ToArray()[0].GetComponent<Text>().text = "FPS: " + 1 / Time.deltaTime;
-
-	// Items
-		// Wood
-			lText.ToArray()[1].GetComponent<Text>().text = "Wood: " + lItem.Find(i => i.sName == "Wood").iCount;
-
-		// Stone
-			lText.ToArray()[2].GetComponent<Text>().text = "Stone: " + lItem.Find(i => i.sName == "Stone").iCount;
-		
-		// Berry
-			lText.ToArray()[3].GetComponent<Text>().text = "Berry: " + lItem.Find(i => i.sName == "Berry").iCount;
-		
-		// Cotton
-			lText.ToArray()[4].GetComponent<Text>().text = "Cotton: " + lItem.Find(i => i.sName == "Cotton").iCount;
-
-		// Raft
-			lText.ToArray()[5].GetComponent<Text>().text = "Raft: " + lItem.Find(i => i.sName == "Raft").iCount;
-	
-	// Clothes
-		// Hat
-			lText.ToArray()[6].GetComponent<Text>().text = "Cotton Hat: " + lItem.Find(i => i.sName == "Cotton Hat").iCount;
-		// Shirt
-			lText.ToArray()[7].GetComponent<Text>().text = "Cotton Shirt: " + lItem.Find(i => i.sName == "Cotton Shirt").iCount;
-		// Pant
-			lText.ToArray()[8].GetComponent<Text>().text = "Cotton PAnt: " + lItem.Find(i => i.sName == "Cotton Pant").iCount;
-			}
+			tFPS.text = "FPS: " + 1 / Time.deltaTime;
+		}
 
 	// Update Pop-up
 		lTransform.ToArray()[0].gameObject.SetActive(rCore.bWin);
